11d9b781c6f013caa03e37f88f95d1ee06c91894
Yiqun Shao <yqshao@ucdavis.edu>
2018-07-25 14:12:45 -0700

    BS updated

--- src/BasisSpecification.jl
+++ src/BasisSpecification.jl
@@ -9,8 +9,8 @@ export BasisSpec #, bsfull, bs_haar, bs_level
     BS = BasisSpec(levlist, levlengths, c2f, description)
 
 is a data structure for a BasisSpec object contaning the following fields:
-* `levlist::Vector{UInt8}`: the integer sequence that specifies a particular basis
-* `levlengths::Vector{UInt16}`: the integer sequence that specifies the length of each basis block in `levlist` (optional)
+* `levlist::Vector{Int}`: the integer sequence that specifies a particular basis
+* `levlengths::Vector{Int}`: the integer sequence that specifies the length of each basis block in `levlist` (optional)
 * `c2f::Bool`: if true (default), this indicates that the basis comes from the coarse-to-fine dictionary; if false, this indicates that the basis comes from the fine-to-coarse dictionary
 * `description::String`: a description of the specified basis
 
@@ -19,6 +19,7 @@ Copyright 2015 The Regents of the University of California
 
 Implemented by Jeff Irion (Adviser: Dr. Naoki Saito) |
 Translated to julia and revised by Naoki Saito, Feb. 22, 2017
+Modified by Yiqun Shao, May. 20, 2018
 """
 type BasisSpec
     levlist :: Vector{Int}
@@ -35,7 +36,7 @@ type BasisSpec
         # Sanity check here.
         if Base.length(levlengths) != Base.length(levlist) && Base.length(levlengths) != 0
             warn("length(levlengths) != length(levlist). Hence, use null levlengths")
-            new( levlist, Vector{Integer}(0), c2f, description )
+            new( levlist, Vector{Int}(0), c2f, description )
         else
             new( levlist, levlengths, c2f, description )
         end
