d8d8f6ab1276cddec130a7682aee3773cca15235
Yiqun Shao <yqshao@ucdavis.edu>
2019-03-22 16:36:16 -0700

    Moved partition matrix functions into GraphPartition.jl

--- src/MTSG.jl
+++ src/MTSG.jl
@@ -13,11 +13,10 @@ include("GHWT_tf_2d.jl")
 include("HGLET.jl")
 include("GraphSig_Plot.jl")
 include("gplot.jl")
-include("PartitionTreeMatrixDhillon.jl")
 
 using Reexport
 @reexport using .GraphSignal, .GraphPartition, .BasisSpecification, .GHWT, .GHWT_2d, .GHWT_tf_1d, .GHWT_tf_2d, .HGLET
 
-export dvec2dmatrix, dmatrix2dvec, levlist2levlengths!, bsfull, bs_haar, bs_level, dvec_Threshold, rs_to_region, GraphSig_Plot, gplot, gplot!, PartitionTreeMatrixDhillon
+export dvec2dmatrix, dmatrix2dvec, levlist2levlengths!, bsfull, bs_haar, bs_level, dvec_Threshold, rs_to_region, GraphSig_Plot, gplot, gplot!
 
 end
