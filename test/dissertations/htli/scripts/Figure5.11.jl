e302c59582fe4f375eab30d48db692e33abc449a
haotian127 <htsli@ucdavis.edu>
2021-04-30 21:50:33 -0700

    larger radii of nodes

--- test/dissertations/htli/scripts/Figure5.11.jl
+++ test/dissertations/htli/scripts/Figure5.11.jl
@@ -1,10 +1,11 @@
 cd(@__DIR__); include("setups/simpletree.jl");
-gr(dpi=200)
+pyplot(dpi=200)
 
 for l = 97:100
+    local plt
     plot(size = (200, 500), framestyle = :none)
     gplot!(W, X, width = 1)
-    scatter_gplot!(X; marker = 𝚽[:, l])
+    scatter_gplot!(X; marker = 𝚽[:, l], ms = 5)
     plt = plot!(cbar = false, clims = (-0.3, 0.3))
     savefig(plt, "../figs/SimpleTree_eigenvector$(l-1)_red.png")
 end
