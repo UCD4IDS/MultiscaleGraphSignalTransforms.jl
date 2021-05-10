b58b3b69e17479e010e7874804799d89a03c00f7
haotian127 <htsli@ucdavis.edu>
2021-05-10 12:06:49 -0700

    zoom in on the cutoff boundaries

--- test/dissertations/htli/scripts/Figure8.9.jl
+++ test/dissertations/htli/scripts/Figure8.9.jl
@@ -12,11 +12,11 @@ T = [(2, 1, 1), (2, 1, 12), (3, 1, 3), (3, 1, 5),
 
 XLs = [
     [-50, 50], [-50, 50], [-110, 40], [-110, 40],
-    [-20, 50], [50, 120], [-80, -30], [-40, 10]
+    [-20, 50], [50, 120], [-80, -20], [-40, -10]
 ]
 YLs = [
     [-50, 50], [-50, 50], [-50, 100], [-50, 100],
-    [-50, 20], [-70, 0], [-100, -50], [-160, -130]
+    [-50, 20], [-70, 0], [-90, -30], [-160, -130]
 ]
 
 for i in 1:length(T)
@@ -24,15 +24,24 @@ for i in 1:length(T)
     𝚽H = HGLET_dic[GP.rs[k, j]:(GP.rs[k + 1, j] - 1), j, :]'
     𝚽lH = LPHGLET_dic[GP.rs[k, j]:(GP.rs[k + 1, j] - 1), j, :]'
     p1 = gplot(W, X; width = 1)
-        scatter_gplot!(X; marker = 𝚽H[:, l], plotOrder = :s2l)
+        scatter_gplot!(X; marker = 𝚽H[:, l], plotOrder = :s2l, ms = 5)
         plot!(xlims = [-180, 220], xlabel = "x(μm)", ylabel = "y(μm)", frame = :box, cbar = false)
         plot!(left_margin = 3mm, bottom_margin = 3mm, clims = (-0.05, 0.05))
     p2 = gplot(W, X; width = 1)
-        scatter_gplot!(X; marker = 𝚽lH[:, l], plotOrder = :s2l)
+        scatter_gplot!(X; marker = 𝚽lH[:, l], plotOrder = :s2l, ms = 5)
         plot!(xlims = [-180, 220], xlabel = "x(μm)", ylabel = "y(μm)", frame = :box, cbar = false)
         plot!(left_margin = 3mm, bottom_margin = 3mm, clims = (-0.05, 0.05))
-    plt = plot(p1, p2, size = (640, 350), layout = Plots.grid(1, 2),
-        # xlims = XLs[i], ylims = YLs[i])
-        xlims = [-120, 120], ylims = [-150, 150])
+    plt = plot(p1, p2, size = (640, 300), layout = Plots.grid(1, 2),
+        xlims = XLs[i], ylims = YLs[i])
+        # xlims = [-120, 120], ylims = [-150, 150])
+    # gplot(W, X; width = 1)
+    # ord = :s2l 
+    # if i == 8
+    #     ord = :l2s
+    # end
+    # scatter_gplot!(X; marker = 𝚽lH[:, l] - 𝚽H[:, l], plotOrder = ord, ms = 4)
+    # plot!(xlabel = "x(μm)", ylabel = "y(μm)", frame = :box, cbar = true)
+    # plot!(left_margin = 3mm, bottom_margin = 3mm, right_margin = 3mm)
+    # plt = plot!(size = (500, 500))
     savefig(plt, "../figs/RGC100_HGLET_LPHGLET_j$(j-1)k$(k-1)l$(l-1).png")
 end
