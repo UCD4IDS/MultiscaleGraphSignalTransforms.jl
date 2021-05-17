8e0a185415929f83f732328f6a2a0ad9e68d2a3a
haotian127 <htsli@ucdavis.edu>
2021-05-17 16:10:16 -0700

    deploy docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,20 @@
 using Documenter, MultiscaleGraphSignalTransforms
 
-makedocs(sitename="MultiscaleGraphSignalTransforms.jl")
+makedocs(
+    sitename="MultiscaleGraphSignalTransforms.jl",
+    format = Documenter.HTML(),
+    authors = "Jeff Irion, Haotian Li, Naoki Saito, Yiqun Shao",
+    clean = true,
+    pages = [
+        "Home" => "index.md",
+        "Functions" => "functions.md",
+    ]
+)
+
+
+# Documenter can also automatically deploy documentation to gh-pages.
+# See "Hosting Documentation" and deploydocs() in the Documenter manual
+# for more information.
+deploydocs(
+    repo = "github.com/haotian127/MultiscaleGraphSignalTransforms.jl.git"
+)
---
+++ docs/src/functions.md
@@ -0,0 +1,6 @@
+## Functions
+
+```@autodocs
+Modules = [MultiscaleGraphSignalTransforms]
+Pages   = ["BasisSpecification.jl", "GHWT.jl", "GHWT_2d.jl", "GHWT_quadtree.jl", "GHWT_tf_1d.jl", "GHWT_tf_2d.jl", "GraphPartition.jl", "GraphSig_Plot.jl", "GraphSignal.jl", "HGLET.jl", "LP-HGLET.jl", "LP-NGWP.jl", "NGWF.jl", "NGWP.jl", "PC-NGWP.jl", "SunFlowerGraph.jl", "VM-NGWP.jl", "common.jl", "dualgraph.jl", "eigDAG_Distance.jl", "eigHAD_Distance.jl", "eigROT_Distance.jl", "eigTSD_Distance.jl", "eigsROT_Distance.jl", "gplot.jl", "helpers.jl", "natural_distances.jl", "partition_fiedler.jl", "unbalanced_haar_image.jl", "utils.jl", "varimax.jl"]
+```
