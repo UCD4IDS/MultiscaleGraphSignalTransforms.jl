13ac60ab7d337f136b06f3f7214c2aab7d9377db
haotian127 <htsli@ucdavis.edu>
2021-05-19 14:34:38 -0700

    export `cost_functional` and `dmatrix_flatten`

--- src/MultiscaleGraphSignalTransforms.jl
+++ src/MultiscaleGraphSignalTransforms.jl
@@ -19,6 +19,7 @@ using Reexport
 @reexport using .GraphSignal, .GraphPartition, .BasisSpecification, .GHWT, .GHWT_2d, .GHWT_tf_1d, .GHWT_tf_2d, .HGLET
 
 export dvec2dmatrix, dmatrix2dvec, levlist2levlengths!, bsfull, bs_haar, bs_level, bs_walsh, dvec_Threshold, rs_to_region, GraphSig_Plot, gplot, gplot!, partition_fiedler
+export cost_functional, dmatrix_flatten
 
 ## export functions of NGWP.jl
 using LinearAlgebra, SparseArrays, LightGraphs, SimpleWeightedGraphs, Clustering
