d1906419ccdd6e238a5b434758357cda51e7cded
haotian127 <htsli@ucdavis.edu>
2021-07-11 17:40:25 -0700

    add docs of `dmatrix_ldb_flatten()`

--- src/utils.jl
+++ src/utils.jl
@@ -200,14 +200,20 @@ end
 """
     function dmatrix_ldb_flatten(dmatrix::Array{Float64,3}...; dm::Symbol = :KLdivergence)
 
-Flatten dmatrix using the LDB method; after this function is called, it becomes
-the size of (~, ~, 1). Example usage: dmatrix_ldb_flatten(dmatrix1, dmatrix2, dmatrix3),
-each argument is the expansion coefficient matrix of a class of signals.
+Flatten dmatrices using the LDB method; after this function is called, it returns
+a matrix of size (~, ~, 1).
 
 ### Input Arguments
-* `dmatrix::Array{Float64,3}`: the matrix of LDB expansion coefficients in one class
-* `dm::Symbol`: discriminant measure. Options: `:KLdivergence`(default),
+* `dmatrix::Array{Float64,3}`: the matrix of LDB expansion coefficients in one class.
+* `dm::Symbol`: discriminant measure. Options: `:KLdivergence` (default),
     `:Jdivergence`, `:l1`, `:l2`, and `:Hellinger`.
+
+### Example Usage:
+`dmatrix_ldb_flatten(dmatrix1, dmatrix2, dmatrix3)`,
+each argument is the expansion coefficient matrix of a class of signals. It uses
+the default discriminant measure KL divergence to flatten these matrices.
+In other words, it flattens these expansion coefficent matrices by computing and
+summing "statistical distances" among them.
 """
 function dmatrix_ldb_flatten(dmatrix::Array{Float64,3}...; dm::Symbol = :KLdivergence)
     C = length(dmatrix) # number of signal classes
